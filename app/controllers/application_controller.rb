03ef21a8238f18c3d2471190d8eec3f877e1bef7
wan <wantingyi@sina.com>
2013-07-24 22:12:01 -0700

    wan
    add relationship bettwen user and post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user,:show_user_posts
 
   def current_user
   	if session[:user_id]
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
   	else
   		@current_user=nil
   	end
+  end
+
+  def show_user_posts
+    Post.where(user_id:@current_user.id)
   end
 
 end
